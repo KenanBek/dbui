30772e92fc57ff2deb3930b84de277193f8006fd
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-27 11:37:07 +0000

    Brew formula update for dbui version v0.1.2

--- Formula/dbui.rb
+++ Formula/dbui.rb
@@ -5,16 +5,16 @@
 class Dbui < Formula
   desc "Interactive terminal user interface and CLI for database connections. MySQL, PostgreSQL. More to come."
   homepage "https://github.com/kenanbek/dbui"
-  version "0.1.1"
+  version "0.1.2"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/KenanBek/dbui/releases/download/v0.1.1/dbui_Darwin_x86_64.tar.gz"
-    sha256 "dff4374311a81afb393ca50d694d5fc86ad6102df099b9498198d5a3bdb5bbeb"
+    url "https://github.com/KenanBek/dbui/releases/download/v0.1.2/dbui_Darwin_x86_64.tar.gz"
+    sha256 "cba11850c2516d18271d746cc58b511634207dd79700960a6a20d34eaa198628"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/KenanBek/dbui/releases/download/v0.1.1/dbui_Linux_x86_64.tar.gz"
-    sha256 "124784010013f1bb32c78eef4db9e3dda1fb4b1572d4554542de8d5f357712f1"
+    url "https://github.com/KenanBek/dbui/releases/download/v0.1.2/dbui_Linux_x86_64.tar.gz"
+    sha256 "84df2980178a7a025e75304ba041ed2a35071a4b73b3bbd15143bbc08252e869"
   end
 
   def install
