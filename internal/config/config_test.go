83ca9a88acca5fed2b8ef6d78f84f2539de95d46
Kanan Rahimov <mail@kenanbek.me>
2021-06-04 23:46:58 +0200

    data source config tests

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -23,5 +23,15 @@ func TestAppConfig_DataSourceConfigs(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Len(t, appConfig.DataSourceConfigs(), 2)
 	assert.Contains(t, appConfig.DataSourceConfigs(), "employees")
+
+	employeesConfig := appConfig.DataSourceConfigs()["employees"]
+	assert.Equal(t, "employees", employeesConfig.Alias())
+	assert.Equal(t, "mysql", employeesConfig.Type())
+	assert.Equal(t, "root:demo@(localhost:3316)/employees", employeesConfig.DSN())
+
 	assert.Contains(t, appConfig.DataSourceConfigs(), "world-db")
+	worldDBConfig := appConfig.DataSourceConfigs()["world-db"]
+	assert.Equal(t, "world-db", worldDBConfig.Alias())
+	assert.Equal(t, "postgresql", worldDBConfig.Type())
+	assert.Equal(t, "user=world password=world123 host=localhost port=5432 dbname=world-db sslmode=disable", worldDBConfig.DSN())
 }
